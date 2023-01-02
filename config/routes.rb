d210ef6a3e8853174416c7e7b715f5053902f5bf
David-Kasilia <davidkasilia001@gmail.com>
2023-01-03 00:27:59 +0300

    Craete a static view home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  
+  root to: 'homepage#index'
+  
   namespace :api do
     namespace :v1 do
       resources :messages
     end
   end
 end
+
+# get 'homepage/index'
