fe3542adba82bb5b3605814d49d03dabb6d7183a
David-Kasilia <davidkasilia001@gmail.com>
2023-01-03 14:13:13 +0300

    Completed static view

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <div class="collapse navbar-collapse justify-content-center" id="navbarCenteredExample">
       <ul class="navbar-nav mb-2 mb-lg-0 d-flex justify-content-between">
         <li class="nav-item">
-             <NavLink to="/" className="link">Home</NavLink>
+             <NavLink to="/" className="nav-link">Home</NavLink>
         </li>
         <li class="nav-item ms-5">
-          <NavLink to="/greeting" className="link">Greeting</NavLink>
+          <NavLink to="/greeting" className="nav-link">Greeting</NavLink>
         </li>
       </ul>
     </div>
--- app/javascript/components/Homepage.js
+++ app/javascript/components/Homepage.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Hello Homepage</h1>
-    </div>
+   <div class="card mt-5 me-5 ms-5">
+  <div class="card-header">
+    Greetings
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>How glorious a greeting the sun gives the mountains!</p>
+      <footer class="blockquote-footer">Source: <cite title="Source Title">John Muir (1997). “Nature Writings: The Story of My Boyhood and Youth, My First Summer in the Sierra, the Mountains of California, Stickeen, Selected Essays”, p.351, Library of America </cite></footer>
+    </blockquote>
+  </div>
+</div>
   )
 }
 
