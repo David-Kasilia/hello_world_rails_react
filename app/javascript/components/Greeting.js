e9f65a6e19a24989441020354386cd44c155ec46
David-Kasilia <davidkasilia001@gmail.com>
2023-01-03 18:26:27 +0300

    Fetch data successfully

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreetingsAsync } from '../redux/greetings/greetings';
 
 const Greeting = () => {
+
+  const { greetings } = useSelector((state) => state.greetings);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetingsAsync());
+  }, []);
+
+  console.log(greetings);
+
   return (
     <div>
       <h2>Greetings from above</h2>
--- app/javascript/redux/store/configureStore.js
+++ app/javascript/redux/store/configureStore.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk'
+import greetingsReducer from '../greetings/greetings'
 
 
 const store = configureStore({
-    reducer: {}, middleware: [thunk],
+    reducer: {
+        greetings: greetingsReducer,
+    }, middleware: [thunk],
 });
 
 export default store;
